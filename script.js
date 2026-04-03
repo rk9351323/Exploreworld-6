5eddf1b19565d2ce026d188ff6af1b57e200d4f0
rk9351323 <rk9351323@gmail.com>
2026-04-03 12:12:30 +0530

    image and ham burger

---
+++ script.js
@@ -0,0 +1,8 @@
+const toggleBtn = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+if (toggleBtn && navLinks) {
+  toggleBtn.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+  });
+}
